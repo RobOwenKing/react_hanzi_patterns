41f45bed9e266d83d4be4f9bbbfeff651d6625be
Robert King <robert.o.king@hotmail.co.uk>
2021-01-18 16:42:15 +0100

    Added: Static LargeCharacter

---
+++ src/components/large_character.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LargeCharacter extends Component {
+  render() {
+    return (
+      <h2>{this.props.char}</h2>
+    );
+  }
+}
+
+export default LargeCharacter;
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <input type="text" className="search-bar" />
+    );
+  }
+}
+
+export default SearchBar;
