5f88ef5fbc1d792ca47ca7eedcf43d739f45d7c8
Robert King <robert.o.king@hotmail.co.uk>
2021-01-27 16:30:52 +0100

    Refactored: Pinyin and Etymology moved to CharacterDetails; Etymology formatting moved out of render() to separate function

--- src/components/character_details.jsx
+++ src/components/character_details.jsx
@@ -3,10 +3,23 @@ import React, { Component } from 'react';
 import LargeCharacter from './large_character.jsx';
 
 class CharacterDetails extends Component {
+  formattedEtymology() {
+    const etymology = this.props.charData.etymology;
+    if (etymology.type !== "pictophonetic") {
+      return `${etymology.type}: ${etymology.hint}`;
+    } else {
+      return `${etymology.phonetic} (phonetic) + ${etymology.semantic} (semantic)`;
+    }
+  };
+
   render() {
     return (
       <div>
         <LargeCharacter charData={this.props.charData} />
+        {this.props.charData.pinyin
+            && <p>{this.props.charData.pinyin.join(', ')}</p>}
+        {this.props.charData.definition && <p>{this.props.charData.definition}</p>}
+        {this.props.charData.etymology && <p>{this.formattedEtymology()}</p>}
       </div>
     );
   }
--- src/components/large_character.jsx
+++ src/components/large_character.jsx
@@ -2,20 +2,9 @@ import React, { Component } from 'react';
 
 class LargeCharacter extends Component {
   render() {
-    const etymology = this.props.charData.etymology;
-    let formattedEtymology;
-    if (etymology.type !== "pictophonetic") {
-      formattedEtymology = `${etymology.type}: ${etymology.hint}`;
-    } else {
-      formattedEtymology = `${etymology.phonetic} (phonetic) + ${etymology.semantic} (semantic)`;
-    }
     return (
       <div>
         <h2>{this.props.charData.character}</h2>
-        {this.props.charData.pinyin
-            && <p>{this.props.charData.pinyin.join(', ')}</p>}
-        {this.props.charData.definition && <p>{this.props.charData.definition}</p>}
-        {this.props.charData.etymology && <p>{formattedEtymology}</p>}
       </div>
     );
   }
