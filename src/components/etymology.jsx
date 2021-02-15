4c89dad0b52902c86d1382c8f9bb5b9be56e468e
Robert King <robert.o.king@hotmail.co.uk>
2021-02-15 15:52:21 +0100

    Added: Pinyin to Etymology

--- src/components/etymology.jsx
+++ src/components/etymology.jsx
@@ -5,13 +5,26 @@ import SmallCharacter from './small_character.jsx';
 import { pinyinify } from '../helpers/pinyinify.js';
 
 class Etymology extends Component {
+  pinyin(char) {
+    const pinyin = this.props.hanzi.getPinyin(char);
+    if (!pinyin) {
+      return '?';
+    } else {
+      const uniques = Array.from(new Set(pinyin));
+      return uniques.map((element) => pinyinify(element)).join(', ');
+    }
+  }
+
   formattedContents() {
     const contents = this.props.contents;
     if (Array.isArray(contents)) {
       return (
         <div>
-          <SmallCharacter char={contents[0]} hanzi={this.props.hanzi} clickHandler={this.props.clickHandler} /> ({contents[1]}) +
-          <SmallCharacter char={contents[2]} hanzi={this.props.hanzi} clickHandler={this.props.clickHandler} />
+          <div>
+            <SmallCharacter char={contents[0]} hanzi={this.props.hanzi} clickHandler={this.props.clickHandler} /> ({contents[1]}) +
+            <SmallCharacter char={contents[2]} hanzi={this.props.hanzi} clickHandler={this.props.clickHandler} /> ({this.pinyin(contents[2])})
+          </div>
+          NB: The pronunciations given are from modern Mandarin, not those at the time the character was created.
         </div>
       );
     } else {
