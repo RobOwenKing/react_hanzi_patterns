6d0e304e39ac5f5b574eb4ff5f7cc97ddd8e982a
Robert King <robert.o.king@hotmail.co.uk>
2021-06-22 15:32:38 +0200

    Refactored: formatDefinition() now calls addSmallCharInStr()

--- src/components/pronunciation.jsx
+++ src/components/pronunciation.jsx
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 
-import { addSmallCharInStr } from '../helpers/add_small_chars_in_str.jsx';
 import { formatDefinition } from '../helpers/format_definition.js';
 import { pinyinify } from '../helpers/pinyinify.js';
 
 class Pronunciation extends Component {
   formatDef(def) {
-    return addSmallCharInStr(formatDefinition(def),
-        this.props.clickHandler,
-        this.props.showPinyin,
-        ""
-      );
-  }
+    return formatDefinition(def, this.props.clickHandler, this.props.showPinyin, "");
+  };
 
   pronunciations() {
     const pros = this.props.charData.pronunciations;
--- src/helpers/format_definition.js
+++ src/helpers/format_definition.js
@@ -1,3 +1,5 @@
-export const formatDefinition = (str) => {
-  return str.replace(/\//g, '; ');
+import { addSmallCharInStr } from './add_small_chars_in_str.jsx';
+
+export const formatDefinition = (str, clickHandler, showPinyin, classes) => {
+  return addSmallCharInStr(str.replace(/\//g, '; '), clickHandler, showPinyin, "");
 };
