d9c4804c4789cce03c7d615ad603e888cf2be7e5
Robert King <robert.o.king@hotmail.co.uk>
2021-06-23 22:04:56 +0200

    Commented: Part of <Etymology />

--- src/components/etymology.jsx
+++ src/components/etymology.jsx
@@ -1,31 +1,62 @@
 import React, { Component } from 'react';
 
+// Used in: showMore()
 import ShowMore from './show_more.jsx';
+// Used in: formatSemantic(), formatPhonetic(), formatNeighbourhoodCell()
 import SmallCharacter from './small_character.jsx';
 
+// Used in: formatHint()
 import { addSmallCharInStr } from '../helpers/add_small_chars_in_str.jsx';
+// Used in: formatPhonetic()
+//          formatNeighbourhood()
 import { getPinyin, fillNeighbourhood } from '../helpers/data.js';
 
+/*
+  Returns: <Etymology />
+  Props:   charData
+           clickHandler
+           showPinyin
+           showMore
+  Used in: <CharacterDetails /> from ./character_details.jsx
+*/
 class Etymology extends Component {
+  /*
+    Returns: String
+             To be used as a section heading
+    Used in: render()
+  */
   etymologyType() {
     if (!this.props.charData.etymology) { return 'Etymology'; }
 
     const type = this.props.charData.etymology.type;
     if (type === 'pictophonetic') {
-      return 'Etymology: Phonosemantic';
+      // I prefer the term phonosemantic over the dataset's pictophonetic
+      return 'Etymology: Phonosemantic Compound';
     } else if (type === 'ideographic') {
       return 'Etymology: Ideographic';
     } else if (type === 'pictographic') {
       return 'Etymology: Pictographic';
     } else {
-      return '';
+      return 'Etymology';
     }
   };
 
+  /*
+    Params:  hint - A string explaining the etymology, possibly containing characters
+             Note: 'hint' is the term used in ../data/dictionary.json
+    Returns: JSX
+             The result of addSmallCharInStr() - the hint with characters as <SmallCharacter />s
+    Used in: etymologyContents()
+  */
   formatHint(hint) {
+    // The <SmallCharacter />s returned are given .char-mid but no margin, being in the middle of sentences
     return addSmallCharInStr(hint, this.props.clickHandler, this.props.showPinyin, "char-mid");
   };
 
+  /*
+    Returns: JSX
+    Used in: formatContents()
+  */
   etymologyContents() {
     if (!this.props.charData.etymology) { return `No data found`; }
 
