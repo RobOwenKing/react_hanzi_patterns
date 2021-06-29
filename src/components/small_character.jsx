ba0fde00c61cc7a9bda9dcefbe77bec3bfec4ba2
Robert King <robert.o.king@hotmail.co.uk>
2021-06-29 23:43:47 +0200

    Commented: <SmallCharacter />

--- src/components/small_character.jsx
+++ src/components/small_character.jsx
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
 
+// Used in pinyin()
 import { getPinyin } from '../helpers/data.js';
+// Used in pinyin()
 import { pinyinify } from '../helpers/pinyinify.js';
 
+/*
+  Returns: <SmallCharacter />
+  Props:   char         - String
+           pinyin       - String. Optional.
+           classes      - Array. Elements are String (CSS classes for the character)
+           clickHandler - Function
+           showPinyin   - Boolean
+  Used in: Many components
+*/
 class SmallCharacter extends Component {
+  /*
+    Params:  event - Event
+    Returns: N/A
+    Action:  Calls clickHandler(), passing it props.char
+    Used in: onClick() in render()
+  */
   handleClick = (event) => {
     this.props.clickHandler(this.props.char);
   };
 
+  /*
+    Returns: String
+             Pinyin corresponding to props.char
+    Used in: component()
+  */
   pinyin = () => {
     if (this.props.pinyin) {
+      // If we've been given pinyin to display
       return pinyinify(this.props.pinyin);
     } else {
+      // If we've been left to look it up for ourselves
       return getPinyin(this.props.char);
     }
   };
 
+  /*
+    Returns: JSX
+             The character in question with pinyin as ruby annotation if props.showPinyin is true
+    Used in: component()
+  */
   component() {
     if (this.props.showPinyin) {
       return (
