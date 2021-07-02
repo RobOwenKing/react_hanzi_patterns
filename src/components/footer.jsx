10c449eb78daae6cbef22b7b7b32ab65ed1f6710
Robert King <robert.o.king@hotmail.co.uk>
2021-07-02 16:53:02 +0200

    Added: <Footer />

---
+++ src/components/footer.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+/*
+  Returns: <Footer />
+  Used in: <App /> from ../App.js
+*/
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>A ReactJS app by Rob Owen King. You can find the code and even comment or contribute on <a href="https://github.com/RobOwenKing/react_hanzi_patterns" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
+        <p>Here's my <a href="http://www.robowenking.com/" target="_blank" rel="noopener noreferrer">website</a>. I'm also on <a href="https://twitter.com/RobOwenKing" target="_blank" rel="noopener noreferrer">Twitter</a>.</p>
+        <p>Built using data from <a href="https://github.com/skishore/makemeahanzi" target="_blank" rel="noopener noreferrer">Make Me A Hanzi</a> under the <a href="https://github.com/skishore/makemeahanzi/blob/master/COPYING" target="_blank" rel="noopener noreferrer">GNU Lesser General Public License</a>.</p>
+        <p>Built using data from <a href="https://github.com/nieldlr/hanzi" target="_blank" rel="noopener noreferrer">HanziJS</a> under the <a href="https://github.com/nieldlr/hanzi/blob/master/LICENSE.txt" target="_blank" rel="noopener noreferrer">MIT License</a>.</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
